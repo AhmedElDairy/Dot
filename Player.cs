23a572b2a3604139ed059a60fb2b94f5fba00da7
AhmedElDairy <58696888+AhmedElDairy@users.noreply.github.com>
2020-10-25 12:00:04 +0100

    Add files via upload
    Dot is a unity 2D based game, it's about a dot that tries to escape hexagons mazes.

---
+++ Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float movspeed = 600f;
+    float movment;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movment = Input.GetAxisRaw("Horizontal");
+    }
+
+    public void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, movment * Time.fixedDeltaTime*- movspeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
